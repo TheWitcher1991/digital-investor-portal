8e3957fc01f65d05e2b719a6e561c674f407f059
TheWitcher1991 <ashot.svazyan@yandex.ru>
2026-05-13 23:48:15 +0300

    feat: end desktop

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,4 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  if (ScrollTrigger.isTouch !== 1) {
+    ScrollSmoother.create({
+      wrapper: ".gs",
+      content: ".content",
+      smooth: 1.5,
+      effects: true,
+    });
+  }
+
+  $("a[href*='#']").on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($(this).attr("href")).offset().top,
+        },
+        777
+      );
+
+    return false;
+  });
+
   const projects = [
     {
       id: 1,
